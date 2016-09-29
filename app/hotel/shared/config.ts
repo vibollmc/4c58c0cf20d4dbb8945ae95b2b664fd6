da4d926af0f2a97a3a9c9232099c161da5887fe5
Cuong Bui <cuongvnm.dh@gmail.com>
2016-09-29 18:05:52 +0700

    A

--- app/hotel/shared/config.ts
+++ app/hotel/shared/config.ts
@@ -1,5 +1,12 @@
+import {md5} from "./md5";
+
 let SystemConfig = {
     apiHost : "http://localhost:8080/api",
+    keyUserId: md5("hms_id"),
+    keyUserName: md5("hms_username"),
+    keyFullName: md5("hms_fullname"),
+    keyUrlImage: md5("hms_urlimage"),
+    keyGroupUser: md5("hms_groupuser")
 }
 
 export default SystemConfig;
